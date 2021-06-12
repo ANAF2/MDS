348aae3f56eeb92f7de3c92620b8550cd72c494d
mihai-radulescu <62609189+mihai-radulescu@users.noreply.github.com>
2021-06-12 14:43:21 +0300

    updated cv-form.php
    made input fields required

--- cv-form.php
+++ cv-form.php
@@ -18,7 +18,7 @@
       <div class="form-group ">
           <label>High school</label>
           <br>
-          <input type="text" name="highschool" class="form-control">
+          <input type="text" name="highschool" class="form-control" placeholder="Where did u go to high school" required>
       </div>    
 
       <br>
@@ -26,7 +26,7 @@
       <div class="form-group">
           <label>University</label>
           <br>
-          <input type="text" name="uni" class="form-control">    
+          <input type="text" name="uni" class="form-control" placeholder="Where did u go to university" required>    
       </div>
 
       <br>
@@ -34,35 +34,35 @@
       <div class="form-group ">
         <label>Work experience</label>
         <br>
-        <input type="text" name="exp" class="form-control" placeholder="What u did">
+        <input type="text" name="exp" class="form-control" placeholder="What u did" required>
 
     </div> 
     
       <div class="form-group ">
         <label>Personal description</label>
         <br>
-        <input type="text" name="desc" class="form-control" placeholder="How are you">
+        <input type="text" name="desc" class="form-control" placeholder="How are you" required>
 
       </div> 
 
       <div class="form-group ">
-        <label>Certifications and diplomas</label>
+        <label>Certifications and diplomas(devide items with ";")</label>
         <br>
-        <input type="text" name="cert" class="form-control" placeholder="How are you">
+        <input type="text" name="cert" class="form-control" placeholder="Certifications" required>
 
       </div> 
 
       <div class="form-group ">
-        <label>Projects</label>
+        <label>Projects(devide items with ";")</label>
         <br>
-        <input type="text" name="projects" class="form-control" placeholder="How are you">
+        <input type="text" name="projects" class="form-control" placeholder="Projects" required>
 
       </div> 
 
       <div class="form-group ">
-        <label>Skills</label>
+        <label>Skills(devide items with ";")</label>
         <br>
-        <input type="text" name="skills" class="form-control" placeholder="How are you">
+        <input type="text" name="skills" class="form-control" placeholder="Skills" required>
 
       </div> 
 
